d3fdc9fde7a6e57b09d75bb57121fadc82deb3f8
yuanoo0 <iiyouyy@163.com>
2025-08-03 16:06:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+''' configs for CIFAR10
+'''
+
+CUDA_VISIBLE_DEVICES = 0
+
+NUM_CLASS = 100
+NUM_TRAIN = 50000
+NUM_VAL   = 50000 - NUM_TRAIN
+BATCH     = 128
+
+TRIALS = 3
+CYCLES = 7
+
+EPOCH = 200
+LR = 0.1
+
+EPOCHL = 160
+SCHEME = 1
+
+MOMENTUM = 0.9
+WDECAY = 5e-4
+
+
+MARGIN = 1.0 # xi
+WEIGHT = 1.0 # lambd
+
+
